21bb7341200da7058cf8f272fe66822f6187ede8
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-05 00:40:58 +0600

    server running

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(4000, () => {
+  console.log(`server running on port 4000`)
+})
 
