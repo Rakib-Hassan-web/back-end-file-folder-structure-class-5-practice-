781170524af2aaef5d7c44508bd601ac09f75e98
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-05 00:45:03 +0600

    working on db

---
+++ dbConfig/index.js
@@ -0,0 +1,11 @@
+
+
+const mongoose = require('mongoose')
+
+const dbconnect =()=>{
+    return mongoose.connect('mongodb+srv://secondwork:tzQJs5WU9fV1tfBw@cluster0.7ooynjm.mongodb.net/?appName=Cluster0')
+  .then(() => console.log(' DB Connected!'));
+
+}
+
+
