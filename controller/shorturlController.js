514f2a251a5a864f59109ffa9deca1d9a47b7da0
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-10 11:50:56 +0600

    shorturl making on process

--- controller/shorturlController.js
+++ controller/shorturlController.js
@@ -1,7 +1,20 @@
+const shortnerSchema = require("../models/shortnerSchema")
 const { isvalidUrl } = require("../utils/validations")
 
 
 
+const randomstring =(length =5)=>{
+    randorstr =""
+    const caracters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+    for( let i= 0 ; i< length ; i++){
+        const randomNum= Math.floor(Math.random() * caracters.length)
+        randorstr +=caracters[randomNum]
+    }
+
+    return randorstr
+
+}
 
 
 
@@ -17,7 +30,17 @@ if(!isvalidUrl(longUrl)) return res.status(400).send('Invalid URL')
 
 
 
+const shortUrl =randomstring()
 
+const urldata = new shortnerSchema({
+    longUrl,shortUrl,
+
+})
+
+urldata.save()
+
+
+res.status(201).send(urldata)
 
 
 
--- routes/shortner.js
+++ routes/shortner.js
@@ -5,6 +5,7 @@ const express = require('express')
 const routerr =express.Router()
 
 
+routerr.post
 
 
 
