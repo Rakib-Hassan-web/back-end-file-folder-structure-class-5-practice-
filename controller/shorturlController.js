89e97a812af430b89225f639bcfeda6e9d744900
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-10 11:34:47 +0600

    short ner working

--- controller/shorturlController.js
+++ controller/shorturlController.js
@@ -7,3 +7,23 @@ const { isvalidUrl } = require("../utils/validations")
 
 
 
+
+const shortnerurl = async (req, res) => {
+
+    const {longUrl} = req.body
+
+    if(!longUrl) return res.status(400).send('longUrl is required')
+if(!isvalidUrl(longUrl)) return res.status(400).send('Invalid URL')
+
+
+
+
+
+
+
+
+
+
+}
+
+
