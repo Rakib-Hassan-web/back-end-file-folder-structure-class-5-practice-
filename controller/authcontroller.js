9bbdf36843a15182d72ab5e434e508a889a4f5eb
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-12-14 23:57:21 +0600

    jwt token packeg install and token create done

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -1,5 +1,9 @@
 const UserSchema = require("../models/UserSchema")
-const { isvalidEmail, isvalidpPasswprd } = require("../utils/validations")
+const jwt = require('jsonwebtoken');
+const {
+    isvalidEmail,
+    isvalidpPasswprd
+} = require("../utils/validations")
 
 
 
@@ -14,19 +18,19 @@ const registration = async (req, res) => {
     } = req.body
 
 
-if(!name) return res.status(400).send('name is required')
-if(!email) return res.status(400).send('email is required')
+    if (!name) return res.status(400).send('name is required')
+    if (!email) return res.status(400).send('email is required')
 
 
 
-    if(!isvalidEmail(email)) return res.status(400).send('Enter a valid email')
+    if (!isvalidEmail(email)) return res.status(400).send('Enter a valid email')
 
-if(!password) return res.status(400).send('password is required')
+    if (!password) return res.status(400).send('password is required')
 
 
-    if(!isvalidEmail(email)) return res.status(400).send('Enter a valid email')
+    if (!isvalidEmail(email)) return res.status(400).send('Enter a valid email')
 
-    if(!isvalidpPasswprd(password)) return res.status(400).send('Enter a valid password')
+    if (!isvalidpPasswprd(password)) return res.status(400).send('Enter a valid password')
 
 
     const existinguser = await UserSchema.findOne({
@@ -55,26 +59,36 @@ if(!password) return res.status(400).send('password is required')
 
 const login = async (req, res) => {
 
-    const { email,password } = req.body
+    const {
+        email,
+        password
+    } = req.body
 
-    if(!email) return res.status(400).send('email is required')
-if(!password) return res.status(400).send('password is required')
+    if (!email) return res.status(400).send('email is required')
+    if (!password) return res.status(400).send('password is required')
 
-    const exisuser = await UserSchema.findOne({ email })
-
-
-    if(!exisuser) return res.status(400).send('user not exist')
-
-
-      const ismatch = await exisuser.comparePassword(password);
-
-      if(!ismatch) return res.status(400).send('password is incorrect')
+    const exisuser = await UserSchema.findOne({
+        email
+    })
 
 
+    if (!exisuser) return res.status(400).send('user not exist')
+
+
+    const ismatch = await exisuser.comparePassword(password);
+
+    if (!ismatch) return res.status(400).send('password is incorrect')
 
 
 
-res.status(200).send({ success :"user login done" , exisuser})
+var token = jwt.sign({ id: exisuser._id , email: exisuser.email }, process.env.JWT_SEC);
+
+console.log(token)
+
+    res.status(200).send({
+        success: "user login done",
+        
+    })
 
 
 
